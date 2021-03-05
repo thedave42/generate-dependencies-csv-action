bdcf375b6216e1d5c80397f1d7ea2cc3eb0d0843
Dave Bloom <dave@digitalnoise.net>
2021-03-05 15:21:52 -0800

    Lets see if this works

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const graphql = github.graphql;
+graphql = graphql.defaults({
+  headers: {
+    authorization: `token ${repoToken}`,
+    Accept: 'application/vnd.github.hawkgirl-preview+json'
+  }
+})
+
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
+  const repoToken = core.getInput('repo-token');
+  const repoName = core.getInput('repo-name');
+  
+  console.log(`repoName is ${repoName}`);
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
