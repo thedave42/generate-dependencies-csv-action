3ffa43cf351a654a95e95407ef729f66b74e05cf
Dave Bloom <dave@digitalnoise.net>
2021-03-05 16:04:43 -0800

    Test

--- dist/index.js
+++ dist/index.js
@@ -11,6 +11,8 @@ const github = __nccwpck_require__(557);
 const repoToken = core.getInput('repo-token');
 const repoName = core.getInput('repo-name');
 
+const [org, repo] = repoName.split('/');
+
 let { graphql } = __nccwpck_require__(380)
 graphql = graphql.defaults({
     headers: {
@@ -19,6 +21,62 @@ graphql = graphql.defaults({
     }
 })
 
+DumpDependencies()
+
+async function DumpDependencies() {
+  let pagination = null
+  const query =
+    `query ($org: String! $repo: String! $cursor: String){
+      repository(owner: $org name: $repo) {
+        name
+        dependencyGraphManifests(first: 100 after: $cursor) {
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+          
+          nodes {
+            dependenciesCount
+            dependencies {
+              nodes {
+                packageManager
+                packageName
+                requirements
+                hasDependencies
+              }
+            }
+          }
+        }
+      }
+    }`
+
+  try {
+    console.log("org,repo,ecosystem,packageName,version,hasDependencies")
+    let hasNextPage = false
+    do {
+      const getDepsResult = await graphql({ query, org: org, repo: repo, cursor: pagination })
+
+      hasNextPage = getDepsResult.repository.dependencyGraphManifests.pageInfo.hasNextPage
+      const repoDependencies = getDepsResult.repository.dependencyGraphManifests.nodes
+
+
+      for (const repoDependency of repoDependencies) {
+        for (const dep of repoDependency.dependencies.nodes) {
+          console.log(`${org},${repo},${dep.packageManager},${dep.packageName},${dep.requirements},${dep.hasDependencies}`)
+        }
+      }
+
+      if (hasNextPage) {
+        pagination = getVulnResult.repository.vulnerabilityAlerts.pageInfo.endCursor
+      }
+    } while (hasNextPage)
+  } catch (error) {
+    console.log('Request failed:', error.request)
+    console.log(error.message)
+  }
+}
+
+/*
 try {
     // `who-to-greet` input defined in action metadata file
 
@@ -31,6 +89,7 @@ try {
 } catch (error) {
     core.setFailed(error.message);
 }
+*/
 
 
 /***/ }),
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const github = require('@actions/github');
 const repoToken = core.getInput('repo-token');
 const repoName = core.getInput('repo-name');
 
+const [org, repo] = repoName.split('/');
+
 let { graphql } = require('@octokit/graphql')
 graphql = graphql.defaults({
     headers: {
@@ -12,6 +14,62 @@ graphql = graphql.defaults({
     }
 })
 
+DumpDependencies()
+
+async function DumpDependencies() {
+  let pagination = null
+  const query =
+    `query ($org: String! $repo: String! $cursor: String){
+      repository(owner: $org name: $repo) {
+        name
+        dependencyGraphManifests(first: 100 after: $cursor) {
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+          
+          nodes {
+            dependenciesCount
+            dependencies {
+              nodes {
+                packageManager
+                packageName
+                requirements
+                hasDependencies
+              }
+            }
+          }
+        }
+      }
+    }`
+
+  try {
+    console.log("org,repo,ecosystem,packageName,version,hasDependencies")
+    let hasNextPage = false
+    do {
+      const getDepsResult = await graphql({ query, org: org, repo: repo, cursor: pagination })
+
+      hasNextPage = getDepsResult.repository.dependencyGraphManifests.pageInfo.hasNextPage
+      const repoDependencies = getDepsResult.repository.dependencyGraphManifests.nodes
+
+
+      for (const repoDependency of repoDependencies) {
+        for (const dep of repoDependency.dependencies.nodes) {
+          console.log(`${org},${repo},${dep.packageManager},${dep.packageName},${dep.requirements},${dep.hasDependencies}`)
+        }
+      }
+
+      if (hasNextPage) {
+        pagination = getVulnResult.repository.vulnerabilityAlerts.pageInfo.endCursor
+      }
+    } while (hasNextPage)
+  } catch (error) {
+    console.log('Request failed:', error.request)
+    console.log(error.message)
+  }
+}
+
+/*
 try {
     // `who-to-greet` input defined in action metadata file
 
@@ -24,3 +82,4 @@ try {
 } catch (error) {
     core.setFailed(error.message);
 }
+*/
